92dec66eb00a13a77cd5d164520a39e303a7d35f
Joaquin Vargas <joaquin.vargascr@outlook.com>
2023-04-13 12:36:07 -0600

    add files to generate docker and the project base

---
+++ Dockerfile
@@ -0,0 +1,72 @@
+FROM php:8.2-cli
+
+EXPOSE 8080
+
+COPY --from=composer:2 /usr/bin/composer /usr/local/bin/
+
+# install the PHP extensions we need
+RUN set -eux; \
+	\
+	if command -v a2enmod; then \
+		a2enmod rewrite; \
+	fi; \
+	\
+	savedAptMark="$(apt-mark showmanual)"; \
+	\
+	apt-get update; \
+	apt-get install -y --no-install-recommends \
+		libfreetype6-dev \
+		libjpeg-dev \
+		libpng-dev \
+		libpq-dev \
+		libwebp-dev \
+		libzip-dev \
+        libicu-dev \
+	; \
+	\
+    docker-php-ext-configure intl ; \
+	docker-php-ext-configure gd \
+		--with-freetype \
+		--with-jpeg=/usr \
+		--with-webp \
+	; \
+	\
+	docker-php-ext-install -j "$(nproc)" \
+        intl \
+		gd \
+		opcache \
+		pdo_mysql \
+		pdo_pgsql \
+		zip \
+	; \
+	\
+# reset apt-mark's "manual" list so that "purge --auto-remove" will remove all build dependencies
+	apt-mark auto '.*' > /dev/null; \
+	apt-mark manual $savedAptMark; \
+	ldd "$(php -r 'echo ini_get("extension_dir");')"/*.so \
+		| awk '/=>/ { print $3 }' \
+		| sort -u \
+		| xargs -r dpkg-query -S \
+		| cut -d: -f1 \
+		| sort -u \
+		| xargs -rt apt-mark manual; \
+	\
+	apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false; \
+	rm -rf /var/lib/apt/lists/*
+
+RUN { \
+                echo 'opcache.enable=1'; \
+		echo 'opcache.memory_consumption=128'; \
+		echo 'opcache.interned_strings_buffer=8'; \
+		echo 'opcache.max_accelerated_files=4000'; \
+		echo 'opcache.revalidate_freq=60'; \
+	} > /usr/local/etc/php/conf.d/opcache-recommended.ini
+
+WORKDIR /var/www/html
+
+ADD composer.json composer.json
+ADD public public
+#
+RUN composer install
+
+CMD php -S 0.0.0.0:8080 -t public public/index.php
---
+++ public/index.php
@@ -0,0 +1,88 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Laminas\Diactoros\Response\JsonResponse;
+use Slim\Exception\HttpNotFoundException;
+use Spatie\SslCertificate\SslCertificate;
+use Spatie\SslCertificate\Exceptions\CouldNotDownloadCertificate\HostDoesNotExist;
+use Iodev\Whois\Factory;
+
+const DATE_FORMAT = 'd/m/Y';
+
+require __DIR__ . '/../vendor/autoload.php';
+
+function cache ($key, $data) {
+    $cache_file = $key . '.txt';
+    $cached_value = file_exists($cache_file) ? file_get_contents($cache_file) : null;
+    if ($cached_value === null || time() - filemtime($cache_file) >= 60) {
+        file_put_contents($cache_file, $data);
+        return $data;
+    } else {
+        return $cached_value;
+    }
+}
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $output = ['output' => 'Hello World..!'];
+
+    return new JsonResponse($output, 200, [], JSON_PRETTY_PRINT);
+});
+
+$app->get('/whois/{domain}', function (Request $request, Response $response, $args) {
+    try {
+        $domain = $args['domain'];
+         if (preg_match("/^[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/", $domain)) {
+             
+            $whois = Factory::get()->createWhois();
+             
+            $info = $whois->loadDomainInfo($domain);
+            
+            $data = unserialize(cache('whois_'.$domain, serialize($info)));
+             
+             if (!is_null($data) && !empty($data)) {
+                $expirationDate = date(DATE_FORMAT, $data->expirationDate);
+                return new JsonResponse(['output' => $expirationDate], 200, [], JSON_PRETTY_PRINT);                 
+             } else {
+                return new JsonResponse(['output' => 'many requests'], 500, [], JSON_PRETTY_PRINT);  
+             }
+         } else {
+            return new JsonResponse(['output' => 'domain is invalid'], 500, [], JSON_PRETTY_PRINT);
+        } 
+    } catch (Exception $exc) {
+        return new JsonResponse(['output' => $exc->getMessage()], 500, [], JSON_PRETTY_PRINT);
+    }
+
+});
+
+$app->get('/ssl/{domain}', function(Request $request, Response $response, $args) {
+    
+    try {
+        $domain = $args['domain'];
+        if (preg_match("/^[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/", $domain)) {
+            $certificate = SslCertificate::createForHostName($domain);
+            $data = unserialize(cache('ssl_'.$domain, serialize($certificate)));
+            $message = ['output' => $data->expirationDate()->isoFormat(DATE_FORMAT)];
+            return new JsonResponse($message, 200, [], JSON_PRETTY_PRINT);
+        } else {
+            return new JsonResponse(['output' => 'domain is invalid'], 500, [], JSON_PRETTY_PRINT);
+        }        
+    } catch (HostDoesNotExist $exc) {
+        return new JsonResponse(['output' => $exc->getMessage()], 500, [], JSON_PRETTY_PRINT);
+    } catch (ErrorException $exc) {
+        return new JsonResponse(['output' => $exc->getMessage()], 500, [], JSON_PRETTY_PRINT);
+    }
+    
+});
+
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
+    throw new HttpNotFoundException($request);
+});
+
+$app->run();
